d048fabaf91b79edc9afd7167f18a7d7116e2196
mohammed <mohammeda.abutaleb@gmail.com>
2025-12-11 13:27:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
